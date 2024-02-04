ec8f6dfc138aaaf624c70da875b8f4d1cfc399b4
pamela-tueam <Pamela.Tueam@campus.lmu.de>
2024-02-04 10:47:54 +0100

    resampling function

---
+++ R/Resampling.R
@@ -0,0 +1,162 @@
+#' @title Resampling S3 Class
+#' 
+#' @description
+#' Split the data into a training and a validation set
+
+#' @param data A Dataset object containing the data
+#' @examples
+#' cars.data <- Dataset(data = cars, target = "dist")
+#' class(cars.data)
+#' @export
+#' 
+Split <- function(data){
+  # Assertion
+  
+  # extract the name of the data
+  name <- data$name
+  # convert the data in a dataframe
+  data <- as.data.frame(data)
+  # the number of row that the data contain
+  nrow <- nrow(data)
+  
+  structure(list(data = data, 
+                 name = name,
+                 nrow = nrow),
+            class = c("Split"))
+}
+
+
+#' @title SplitCV method for splitting the data
+#' 
+#' @description Split the data into 2 parts: the training and validation part
+#' 
+#' @param folds A numeric value: give the number of folds
+#' @param repeats A numeric value: the number of repetition
+#' @examples
+#' cars.data <- Dataset(data = cars, target = "dist")
+#' cars.data[c(1, 2, 3, 4), ]
+#' cars.data[c(1, 2), "dist"]
+#' @export
+SplitCV <- function(folds, repeats = 1){
+  # Input checks
+  assertNumeric(folds)
+  assertNumeric(repeats)
+  
+  # define the split function
+  Spl <- function(data){
+    
+    x <- Split(data)
+    
+    nbr_row <- x$nrow
+    
+    # the train indices
+    n_train <- nbr_row/folds
+    # the validation indices
+    n_validation <- nbr_row - n_train
+    
+    # the list of all splits
+    all_split <- vector("list", length = folds)
+    
+    for(i in seq_len(folds)) {
+      
+      training <- sample(nbr_row, size = n_train, replace = FALSE)
+      validation <- setdiff(seq_len(nbr_row), training)
+      all_split[[i]] <- list(training = training,
+                          validation = validation)
+    }
+    
+    # add futher arguments to the all_split list
+    all_split[["folds"]] <- folds
+    all_split[["repeats"]] <- repeats
+    all_split[["name"]] <- x$name
+    all_split[["nbr_row"]] <- nbr_row
+    
+    structure(all_split,
+    class = c("SplitInstanceCV", "SplitInstance")
+    )
+  }
+  class(Spl) = c("SplitCV", "Split")
+  return(Spl)
+}
+
+#' set the class of the SplitCV object
+class(SplitCV) <- c("SplitConstructorCV", "SplitConstructor", "function")
+
+#' Create an environment for storing Split objects
+splt <- new.env()
+
+#' store the SplitCV object cv to the environment
+splt$cv <- SplitCV
+
+
+#' @title 'print' method for SplitCV objects
+#' 
+#' @description Print a SplitCV object
+#' 
+#' @param x A SplitInstance objec
+#' @param ... Additional arguments
+#' @examples
+#' cars.split <- cv5(cars.data)
+#' print(cars.data)
+#' @export
+#' 
+print.SplitInstance <- function(x){
+  cat("CV Split Instance of the \"", x$name, ", dataset (", x$nbr_row, " rows)\n", sep = "")
+  cat("Configuration: folds = ", x$folds, ", repeats = ", x$repeats, "\n", sep = "")
+  invisible(x)
+}
+
+
+##' @title 'hypaerparameters' generic function
+#' 
+#' @description Get information about the hyperparameters of an object
+#' 
+#' @param x An object
+#' @examples
+#' hyperparameters(splt$cv)
+#' @export
+hyperparameters <- function(x){
+  UseMethod("hyperparameters")
+}
+
+#' @title 'hypermarameters' method for SplitCV objects
+#' 
+#' @description Get the hyperparameters information of a Split object
+#' 
+#' @param x A SplitCV/SplitConstructorCV object
+#' @examples
+#' hyperparameters(splt$cv)
+#' @export
+hyperparameters.SplitConstructorCV <- function(x){
+  cat("Hyperparameter Space: \n")
+  data.frame(name = c("folds", "repeats"),
+                type = c("int", "int"),
+                range = c("[2, Inf]", "[1, Inf]"))
+}
+
+
+
+#' @title 'length' method for "SplitInstanceCV"/"SplitInstance" objects
+#' 
+#' @description Get the length/the number of Split of a "SplitInstanceCV" "SplitInstance" object
+#' 
+#' @param x A SplitCV/SplitConstructorCV object
+#' @examples
+#' length(cars.split)
+#' @export
+length.SplitInstance <- function(x){
+  return(x$folds)
+}
+
+#' Example
+#' 
+cars.data <- Dataset(data = cars, target = "dist")
+identical(splt$cv, SplitCV)
+hyperparameters(splt$cv)
+cv5 <- splt$cv(folds = 5)
+class(cv5)
+cars.split <- cv5(cars.data)
+cars.split
+class(cars.split)
+length(cars.split)
+cars.split[[1]]
